b3a6d18726063c0291fee37d0bd7fe7b85d50e86
Eyüp Can Yönter <oniraca.om00@gmail.com>
2024-01-08 17:13:50 +0300

    kill me

--- eduflow/src/main/java/com/final_project/Config/SecurityConfig.java
+++ eduflow/src/main/java/com/final_project/Config/SecurityConfig.java
@@ -1,24 +1,43 @@
 package com.final_project.Config;
 
-import com.final_project.DataAccess.Repositories.StudentRepository;
+import com.final_project.services.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
-
-    private final StudentRepository studentRepository;
-
-    public SecurityConfig(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+public class SecurityConfig {
+    private final AuthService authService;
+    public SecurityConfig(UserService authService) {
+        this.authService = authService;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http = http.cors().and().csrf().disable();
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authService(authService);
+        return authenticationManagerBuilder.build();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http = http.cors().and().csrf().disable();
+//    }
 }
